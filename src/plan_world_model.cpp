e7db58aae2fa14de2ab71cb24f176b94b05449be
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2024-05-27 10:47:51 +0700

    test binding (fix x2)

--- src/plan_world_model.cpp
+++ src/plan_world_model.cpp
@@ -1,21 +1,21 @@
-#include "plan_world_model.h"
-#include                   <godot_cpp/core/class_db.hpp>
-
-using namespace
-          godot;
-
-
-PlanWorldModel:: PlanWorldModel()
-{
-    this->possible_actions = new TypedArray<Ref<PlanAction>>();
-    this->           goals = new TypedArray<Ref<PlanGoal  >>();
-}
-
-PlanWorldModel::~PlanWorldModel()
-{
-}
-
-void
-PlanWorldModel::_bind_methods()
-{
-}
+//#include "plan_world_model.h"
+//#include                   <godot_cpp/core/class_db.hpp>
+//
+//using namespace
+//          godot;
+//
+//
+//PlanWorldModel:: PlanWorldModel()
+//{
+//    this->possible_actions = new TypedArray<Ref<PlanAction>>();
+//    this->           goals = new TypedArray<Ref<PlanGoal  >>();
+//}
+//
+//PlanWorldModel::~PlanWorldModel()
+//{
+//}
+//
+//void
+//PlanWorldModel::_bind_methods()
+//{
+//}
--- src/plan_world_model.h
+++ src/plan_world_model.h
@@ -1,46 +1,46 @@
-#ifndef PLAN_WORLD_MODEL_H
-#define PLAN_WORLD_MODEL_H
-
-#ifdef WIN32
-#include <windows.h>
-#endif
-
-#include <godot_cpp/classes/resource.hpp>
-#include                                <godot_cpp/variant/typed_array.hpp>
-#include                                                                  <godot_cpp/classes/ref.hpp>
-
-#include "plan_goal.h"
-#include "plan_action.h"
-
-namespace godot
-{
-    class PlanWorldModel : public Resource
-    {
-
-  GDCLASS(PlanWorldModel ,        Resource)
-
-protected:
-
-          static void _bind_methods();
-
-
-
-          int   actions_pointer   ;
-           bool actions_discovered;
-          float passing_timing    ;
-
-          TypedArray<Ref<PlanAction>> possible_actions;
-          TypedArray<Ref<PlanGoal  >>            goals;
-public   :
-
-          PlanWorldModel();
-         ~PlanWorldModel();
-
-          virtual void apply_action(PlanAction action) = 0;
-          virtual
-          PlanWorldModel       copy(                 ) = 0;
-          virtual void 
-    };
-}
-
-#endif//PLAN_WORLD_MODEL_H
+//#ifndef PLAN_WORLD_MODEL_H
+//#define PLAN_WORLD_MODEL_H
+//
+//#ifdef WIN32
+//#include <windows.h>
+//#endif
+//
+//#include <godot_cpp/classes/resource.hpp>
+//#include                                <godot_cpp/variant/typed_array.hpp>
+//#include                                                                  <godot_cpp/classes/ref.hpp>
+//
+//#include "plan_goal.h"
+//#include "plan_action.h"
+//
+//namespace godot
+//{
+//    class PlanWorldModel : public Resource
+//    {
+//
+//  GDCLASS(PlanWorldModel ,        Resource)
+//
+//protected:
+//
+//          static void _bind_methods();
+//
+//
+//
+//          int   actions_pointer   ;
+//           bool actions_discovered;
+//          float passing_timing    ;
+//
+//          TypedArray<Ref<PlanAction>> possible_actions;
+//          TypedArray<Ref<PlanGoal  >>            goals;
+//public   :
+//
+//          PlanWorldModel();
+//         ~PlanWorldModel();
+//
+//          virtual void apply_action(PlanAction action) = 0;
+//          virtual
+//          PlanWorldModel       copy(                 ) = 0;
+//          virtual void 
+//    };
+//}
+//
+//#endif//PLAN_WORLD_MODEL_H
