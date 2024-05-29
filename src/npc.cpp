0b05299aaf70e5151e0b15e56fa6102f3f28757c
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2024-05-29 18:55:39 +0700

    test

--- src/npc.cpp
+++ src/npc.cpp
@@ -47,7 +47,7 @@ try
              if  (current_action == nullptr
              || !(current_action -> are_precondition_collection_met(world_state.ptr())))
              {
-godot::_err_print_error("_notification", "npc.cpp", 48, "ok 1", false, false);
+//godot::_err_print_error("_notification", "npc.cpp", 48, "ok 1", false, false);
                  //action_queue = goap_planner.ptr()->plan           (world_state.ptr())  ;
 //             if (!action_queue . is_empty())
 //                 {
