d5d8b6f33a69373e903cff7085e24972ebb44ba8
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2024-05-29 17:25:49 +0700

    test

--- src/world_state.cpp
+++ src/world_state.cpp
@@ -62,7 +62,7 @@ const  Dictionary&    required_state_collection    ) const
                       required_state_collection.keys();
     for (int index = 0; index < keys.size();  ++index)
     {
-     if (             required_state_collection.get(keys[index], 0)
+     if (required_state_collection.has(keys[index]) && state_collection.has(keys[index]) &&             required_state_collection.get(keys[index], 0)
      !=                        state_collection.get(keys[index], 0)
         )
         {
